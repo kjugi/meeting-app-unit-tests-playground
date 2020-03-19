3a940d2666a6e996a5f3c824f17062217c90d623
Filip <filip.kudla@snow.dog>
2020-03-19 22:30:37 +0100

    Add /user GET with 2s delay response

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,18 +1,37 @@
 var appRouter = function (app) {
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-      });
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
-    app.post("/add", function (req, res) {
-        // TODO: add random error response
-        res.status(200).send({
-            status: true
-        });
+  app.post('/add', function (req, res) {
+    // TODO: add random error response
+    res.status(200).send({
+      status: true
     });
+  });
 
-    // TODO: add list of predefined users
+  app
+    .use('/users', function(req, res, next) {
+      setTimeout(next, 2000)
+    })
+    .get('/users', function (req, res) {
+      res.status(200).send([
+        {
+          text: 'test',
+          value: 'example@o2.pl'
+        },
+        {
+          text: 'Example Person1',
+          value: 'meeting1@gmail.com'
+        },
+        {
+          text: 'Example Person2',
+          value: 'meeting2@gmail.com'
+        }
+      ]);
+    });
 }
 
 module.exports = appRouter;
