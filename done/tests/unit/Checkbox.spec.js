97fc35f539eb5e8fa547e13616e3a7dddae25960
Filip <filip.kudla@snow.dog>
2020-03-21 00:20:07 +0100

    Add Checkbox.spec file as start of unit tests for this copmonent

---
+++ done/tests/unit/Checkbox.spec.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import Checkbox from '@/components/Checkbox.vue'
+
+describe('Checkbox.vue', () => {
+  it('renders prop title when passed', () => {
+    const wrapper = mount(Checkbox, {
+      propsData: {
+        //
+      }
+    })
+  })
+})
