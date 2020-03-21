b72776d4e6e6d563ad359013e8b3001e0cb76515
Filip <filip.kudla@snow.dog>
2020-03-21 22:53:51 +0100

    Finish unit test for Message component

--- done/tests/unit/Message.spec.js
+++ done/tests/unit/Message.spec.js
@@ -1,16 +1,36 @@
 import { mount } from '@vue/test-utils'
 import Message from '@/components/Message.vue'
 
+jest.useFakeTimers()
+
 describe('Message.vue', () => {
   it('renders prop title when passed', () => {
     const wrapper = mount(Message, {
       propsData: {
-        title: 'test title'
+        title: 'test title',
+        isMessageShowed: false
       }
     })
 
     expect(wrapper.find('h2').text()).toMatch('test title')
   })
 
-  // it('emit event when isMessageShowed updates', () => {})
+  it('emit event when isMessageShowed updates', async () => {
+    const wrapper = mount(Message, {
+      propsData: {
+        title: 'test title',
+        isMessageShowed: false
+      }
+    })
+
+    expect(wrapper.emitted()).toEqual({})
+    expect(wrapper.emitted()).toEqual(expect.not.objectContaining({ hideMessage: [[]] }))
+
+    await wrapper.setProps({ isMessageShowed: true })
+    jest.runAllTimers()
+
+    expect(wrapper.emitted('hideMessage')).toBeDefined()
+    expect(wrapper.emitted('hideMessage').length).toBe(1)
+    expect(wrapper.emitted('hideMessage')[0]).toEqual([])
+  })
 })
