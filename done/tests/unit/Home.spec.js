4b9db6f5f007c1908d10bddcbedcb03707d84511
Filip <filip.kudla@snow.dog>
2020-03-22 00:23:09 +0100

    Finish unit test for Home view

--- done/tests/unit/Home.spec.js
+++ done/tests/unit/Home.spec.js
@@ -1,1 +1,45 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import HomePage from '@/views/Home.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('Home page', () => {
+  it('meeting list contains elements from store', () => {
+    const state = {
+      meetingList: [
+        {
+          'date': '2020-03-27',
+          'hour': false,
+          'who': 'Example Person1'
+        },
+        {
+          'date': '2020-03-27',
+          'hour': '9:50',
+          'who': 'Example Person2'
+        }
+      ]
+    }
+
+    const store = new Vuex.Store({
+      state
+    })
+
+    const wrapper = mount(HomePage, { store, localVue })
+
+    expect(wrapper.findAll('.home__item')).toHaveLength(2)
+  })
+
+  it('render empty list message when don\'t have items', () => {
+    const store = new Vuex.Store({
+      state: {
+        meetingList: []
+      }
+    })
+
+    const wrapper = mount(HomePage, { store, localVue })
+
+    expect(wrapper.find('.home div').text()).toBe('Meeting list is empty!')
+  })
+})
