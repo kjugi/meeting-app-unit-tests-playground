9de3e7ee4a1b4e3ad0e47cb02de22b1484050423
Filip <filip.kudla@snow.dog>
2020-03-24 23:29:48 +0100

    Update practices Home unit test with necessary imports

--- practices/tests/unit/Home.spec.js
+++ practices/tests/unit/Home.spec.js
@@ -1,5 +1,9 @@
 import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
 import HomePage from '@/views/Home.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Home page', () => {
   // it('meeting list contains elements from store', () => {
