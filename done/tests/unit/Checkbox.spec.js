5ccdb35223ed94ccab6c38611b813ab8d30409f6
Filip <filip.kudla@snow.dog>
2020-03-24 23:20:05 +0100

    Update Checkbox unit test and fix problem with wrong expectation

--- done/tests/unit/Checkbox.spec.js
+++ done/tests/unit/Checkbox.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import Checkbox from '@/components/Checkbox.vue'
 
 describe('Checkbox.vue', () => {
-  it('emit events when trigger change on checkbox', () => {
+  it('emit events when change checked value', async () => {
     const wrapper = mount(Checkbox, {
       propsData: {
         id: 'checkbox1',
@@ -11,16 +11,13 @@ describe('Checkbox.vue', () => {
       }
     })
 
-    expect(wrapper.vm.localValue).toBe(wrapper.props().value)
-
-    wrapper.find('#checkbox1').trigger('change')
+    await wrapper.find('#checkbox1').setChecked()
 
     expect(wrapper.emitted('change')).toBeDefined()
     expect(wrapper.emitted('change').length).toBe(1)
     expect(wrapper.emitted('change')[0]).toEqual([])
     expect(wrapper.emitted('input')).toBeDefined()
     expect(wrapper.emitted('input').length).toBe(1)
-    expect(wrapper.emitted('input')[0][0]).toEqual(false)
-    expect(wrapper.vm.localValue).not.toBe(!wrapper.props().value)
+    expect(wrapper.emitted('input')[0][0]).toEqual(true)
   })
 })
