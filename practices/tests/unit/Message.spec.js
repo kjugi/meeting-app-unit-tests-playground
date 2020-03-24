b16166db434f214270694cb2a37c7fff69c96637
Filip <filip.kudla@snow.dog>
2020-03-24 23:58:17 +0100

    Add commented jest method in practice message component

--- practices/tests/unit/Message.spec.js
+++ practices/tests/unit/Message.spec.js
@@ -3,6 +3,7 @@ import Message from '@/components/Message.vue'
 
 // Check for more: https://jestjs.io/docs/en/timer-mocks
 jest.useFakeTimers()
+// jest.runAllTimers()
 
 describe('Message.vue', () => {
   it('renders prop title when passed', () => {
