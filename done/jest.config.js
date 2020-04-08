e7b46a8b2bac6f67d27ed5c04162af5148e98126
Filip <filip.kudla@snow.dog>
2020-04-08 23:42:22 +0200

    Add jest-date-mock lib to done directory

--- done/jest.config.js
+++ done/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   snapshotSerializers: [
-    "<rootDir>/node_modules/jest-serializer-vue"
+    '<rootDir>/node_modules/jest-serializer-vue'
+  ],
+  setupFiles: [
+    'jest-date-mock'
   ]
 }
