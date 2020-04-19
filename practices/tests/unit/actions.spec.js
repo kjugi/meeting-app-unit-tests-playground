c5f24304ee7935d20859fe03a1d1a01470bda27f
Filip <filip.kudla@snow.dog>
2020-04-19 15:14:43 +0200

    Update practices test cases for actions

--- practices/tests/unit/actions.spec.js
+++ practices/tests/unit/actions.spec.js
@@ -3,4 +3,7 @@ import { actions } from '@/store'
 describe('actions - store', () => {
   // it('doReservation commits to mutation', () => {
   // })
+
+  // it('fakeApiAction makes commit based on API response', async () => {
+  // })
 })
